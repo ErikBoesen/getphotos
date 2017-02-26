7a2bc26ea4dda68b98fd93eb6b182f7a1383f180
Erik Boesen <ErikBoesen@users.noreply.github.com>
2017-02-26 14:02:06 -0500

    Automatically make Phone folder

--- getphotos.sh
+++ getphotos.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+mkdir ~/Desktop/Phone
 # TODO: Use backslashes before {}s to override asking password multiple times. Currently broken for Android shell
 scp -rP 2222 erik@192.168.1.170:/storage/emulated/0/{DCIM/Camera/,Pictures/Messenger/,Pictures/Screenshots/,Kik/,Download/} ~/Desktop/Phone/
