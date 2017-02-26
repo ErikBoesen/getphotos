153a9fd059293444009f52e2342cf3fedd6d51aa
Erik Boesen <ErikBoesen@users.noreply.github.com>
2017-02-26 13:48:51 -0500

    Add files via upload

---
+++ getphotos.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# TODO: Use backslashes before {}s to override asking password multiple times. Currently broken for Android shell
+scp -rP 2222 erik@192.168.1.170:/storage/emulated/0/{DCIM/Camera/,Pictures/Messenger/,Pictures/Screenshots/,Kik/,Download/} ~/Desktop/Phone/
