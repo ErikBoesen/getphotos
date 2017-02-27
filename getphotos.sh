fd8f3f5b97a7600ebb069117b0cd4c15bc2f7b62
Erik Boesen <ErikBoesen@users.noreply.github.com>
2017-02-26 21:53:14 -0500

    Create multiple folders, indexed by date.
    Folder will be deleted after seven days.

--- getphotos.sh
+++ getphotos.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-mkdir ~/Desktop/Phone
-# TODO: Use backslashes before {}s to override asking password multiple times. Currently broken for Android shell
-scp -rP 2222 erik@192.168.1.170:/storage/emulated/0/{DCIM/Camera/,Pictures/Messenger/,Pictures/Screenshots/,Kik/,Download/} ~/Desktop/Phone/
+THEN=`date --date="1 week ago" +%y-%m-%d`
+NOW=`date +%y-%m-%d`
+rm -rf ~/Phone/$THEN
+mkdir -p ~/Phone/$NOW
+
+scp -rP 2222 erik@192.168.1.170:/storage/emulated/0/{DCIM/Camera/,Pictures/Messenger/,Pictures/Screenshots/,Kik/,Download/} ~/Phone/$NOW
